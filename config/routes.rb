1f0831359b3794bb7b3218175021dc7f1c12fad2
Shunta-Ono <nisikoubaseball.a@softbank.ne.jp>
2020-03-08 14:34:43 +0000

    [update]routingの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,7 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    get 'homes/top'
-  end
-  get 'homes/top'
-  namespace :admin do
-    get 'muscles/new'
-    get 'muscles/index'
-    get 'muscles/show'
-    get 'muscles/edit'
-  end
-  namespace :admin do
-    get 'genres/new'
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  get 'notes/new'
-  get 'notes/index'
-  get 'notes/show'
-  get 'notes/edit'
-  get 'genres/index'
-  get 'muscles/index'
-  get 'muscles/show'
-  get 'users/show'
+  root 'homes#top'
+
   devise_for :admins, controllers: {
     sessions: "admin/devise/sessions",
     passwords: "admin/devise/passwords",
@@ -34,5 +13,17 @@ Rails.application.routes.draw do
     passwords: "users_devise/passwords",
     registrations: "users_devise/registrations"
   }
+  #admin
+  namespace :admin do
+    resources :muscles
+    resources :genres, except: [:show]
+  end
+
+  #user
+  get 'homes/top'
+  resources :muscles, only: [:index,:show]
+  resources :genres, only: [:index]
+  resources :notes
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
