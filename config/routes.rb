a79fd1ab275402e3e24846c17301305c395bafc6
Shunta-Ono <nisikoubaseball.a@softbank.ne.jp>
2020-03-08 12:10:28 +0000

    [create]deviseによるログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+  devise_for :admins, controllers: {
+    sessions: "admin/devise/sessions",
+    passwords: "admin/devise/passwords",
+    registrations: "admin/devise/registrations"
+  }
+
+  devise_for :users, controllers: {
+    sessions: "users_devise/sessions",
+    passwords: "users_devise/passwords",
+    registrations: "users_devise/registrations"
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
