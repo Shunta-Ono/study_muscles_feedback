9141d74fa49d5ccc3d81e06c407c0abfa65a80a4
Shunta-Ono <nisikoubaseball.a@softbank.ne.jp>
2020-03-08 17:01:52 +0000

    update[adminログイン後の遷移先変更]

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,2 +1,11 @@
 class Admin::BaseController < ApplicationController
+
+  def after_sign_in_path_for(resource)
+    admin_genres_path
+  end
+
+  def after_sign_up_path_for(resource)
+    admin_genres_path
+  end
+
 end
