35e887b0461341cc595e60eb110ebbc945f1b166
Shunta-Ono <nisikoubaseball.a@softbank.ne.jp>
2020-03-09 13:59:40 +0000

    [create]admin-genre-index,edit

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -11,15 +11,24 @@ class Admin::GenresController < Admin::BaseController
   end
 
   def index
+    @genre = Genre.new
+    @genres = Genre.all
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to admin_genres_path
   end
 
   def destroy
+    genre = Genre.find(params[:id])
+    genre.delete
+    redirect_to admin_genres_path
   end
 
   private
