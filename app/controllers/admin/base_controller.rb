bb46b2149ca7df859362cf39dd86ccd24a97e6ef
Shunta-Ono <nisikoubaseball.a@softbank.ne.jp>
2020-03-09 07:02:41 +0000

    [create]adminとuserのheader

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,11 +1,6 @@
 class Admin::BaseController < ApplicationController
 
-  def after_sign_in_path_for(resource)
-    admin_genres_path
-  end
-
-  def after_sign_up_path_for(resource)
-    admin_genres_path
-  end
+  # adminのlayout指定
+  layout 'admin'
 
 end
