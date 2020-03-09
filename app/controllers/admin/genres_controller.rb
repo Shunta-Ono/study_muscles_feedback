9d1c9bbc7f3b674c42966e4179a7154c59725e22
Shunta-Ono <nisikoubaseball.a@softbank.ne.jp>
2020-03-09 09:37:14 +0000

    [create]genre作成機能

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,9 +1,13 @@
 class Admin::GenresController < Admin::BaseController
 
   def new
+    @genre = Genre.new
   end
 
   def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to admin_genres_path
   end
 
   def index
@@ -16,6 +20,11 @@ class Admin::GenresController < Admin::BaseController
   end
 
   def destroy
+  end
+
+  private
+  def genre_params
+    params.require(:genre).permit(:name, :image)
   end
 
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,4 @@
 class Genre < ApplicationRecord
   has_many :muscles, dependent: :destroy
+  attachment :image
 end
