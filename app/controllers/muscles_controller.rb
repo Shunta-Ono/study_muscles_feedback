e4c420a76f1b68b7b2637b84da87a33d3c347cef
Shunta-Ono <nisikoubaseball.a@softbank.ne.jp>
2020-03-16 22:43:00 +0000

    [create]user-genre-index,muscle-index,muscle-show

--- app/controllers/muscles_controller.rb
+++ app/controllers/muscles_controller.rb
@@ -1,7 +1,10 @@
 class MusclesController < ApplicationController
   def index
+    @muscles = Muscle.where(genre_id: params[:genre_id])
+    @genres = Genre.all
   end
 
   def show
+    @muscle = Muscle.find{params[:id]}
   end
 end
