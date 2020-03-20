435885d99296b73d1437b492c232ef24cde5bc59
Shunta-Ono <nisikoubaseball.a@softbank.ne.jp>
2020-03-20 04:27:09 +0000

    user-controllerのbefore-action設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :correct_user
+
   def show
     @user = User.find(params[:id])
     @notes = Note.all
     @note = @user.notes
   end
+
+  private
+
+  def correct_user
+    @user = User.find(params[:id])
+    unless @user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
+  end
+
 end
