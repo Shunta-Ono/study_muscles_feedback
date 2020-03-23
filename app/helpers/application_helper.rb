fa53a7ec86a16a2d79de5b2061469ccd047eb7fd
Shunta-Ono <nisikoubaseball.a@softbank.ne.jp>
2020-03-23 16:10:28 +0000

    preview-function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def muscle_action_label(num)
+    num.zero? ? "動作前" : "動作後"
+  end
+
+  def select_muscle_image_label(num)
+    "#{muscle_action_label(num)}の筋肉を選択してください"
+  end
 end
